f9ce6a6559b470bb8a5afb411d4895ffceba25df
Mustapha Muhammad Garba <mgarba88@gmail.com>
2019-01-01 23:36:41 +0100

    edited to correct error

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,35 +1,63 @@
-import React, {  Component  } from 'react';
-import Task from './Task';
 
-//component for the whole app
+import React, { Component } from 'react';
+import Task from './Task.js';
+
+
 export default class App extends Component {
-    getTasks(){
-        return [
-            { _id=1, text="This is task 1"},
-            { _id=2, text="This is task 2"},
-            { _id=3, text="This is task 3"},
-        ];
-    }
-    renderTasks(){
-        return this.getTasks().map((task) => (
-            <Task key={task._id} task={task} />
-        ));
 
-    }
+  getTasks() {
 
-    render(){
-        return(
-            <div className="Container">
-                <header>
-                    <h1>To-Do List</h1>
-                </header>
+    return [
 
-                <ul>
-                    {this.renderTasks()}
-                </ul>
+      { _id: 1, text: 'This is task 1' },
 
-            </div>
+      { _id: 2, text: 'This is task 2' },
 
-        );
-    }
+      { _id: 3, text: 'This is task 3' },
+
+    ];
+
+  }
+
+
+
+  renderTasks() {
+
+    return this.getTasks().map((task) => (
+
+      <Task key={task._id} task={task} />
+
+    ));
+
+  }
+
+
+
+  render() {
+
+    return (
+
+      <div className="container">
+
+        <header>
+
+          <h1>Todo List</h1>
+
+        </header>
+
+
+
+        <ul>
+
+          {this.renderTasks()}
+
+        </ul>
+
+      </div>
+
+    );
+
+  }
+
 }
+
